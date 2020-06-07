ed4ec1029e7ac8e4ad7a49487021bbfc7b63509c
Jason Hur <jasonhur@Jasons-MacBook-Pro.local>
2020-06-06 19:04:58 -0700

    ready for turn in

--- controllers/articles.js
+++ controllers/articles.js
@@ -63,10 +63,8 @@ function index(req, res) {
 function create(req, res) {
     req.body.user = req.user;
     const article = new Article(req.body);
-    console.log(req.body);
     article.save(function (err) {
         if (err) return res.redirect('/articles');
-        console.log(article);
         res.redirect(`/articles`);
     });
 }
